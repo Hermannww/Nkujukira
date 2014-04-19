1fab438fc145452c3094cdd7bdc80f5b35cbd52c
nkasozi <nkasozi@gmail.com>
2014-04-20 01:08:22 +0300

    Nkujukira source code as it is.only 3 of 6 component has been implemented. these are footage grabbing component face detection component display updater component Up next alert generation component then there is Face recognition component User authentication component

---
+++ MetroFramework.Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MetroFramework.Demo
+{
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            try
+            {
+                Application.Run(new MainWindow());
+            }
+            catch (ArgumentException e)
+            {
+
+            }
+        }
+    }
+}
